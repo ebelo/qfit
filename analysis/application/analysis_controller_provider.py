550863f8b574b53ae7cadd96064a8038a1a10cbe
ebelo <emmanuel.belo@camptocamp.com>
2026-04-13 07:18:11 +0200

    refactor: add analysis workflow port

--- analysis/application/analysis_controller_provider.py
+++ analysis/application/analysis_controller_provider.py
@@ -1,4 +1,7 @@
-def build_analysis_controller():
+from .analysis_workflow_port import AnalysisWorkflowPort
+
+
+def build_analysis_controller() -> AnalysisWorkflowPort:
     from .analysis_controller import AnalysisController
 
     return AnalysisController()
