fb52524e9c12be59009230c3d657cd47b1746b96
Emmanuel Belo <emmanuel.belo@camptocamp.com>
2026-04-13 06:05:01 +0200

    refactor: extract analysis mode dispatch (#520)

--- analysis/application/analysis_controller.py
+++ analysis/application/analysis_controller.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 from ...activities.application.activity_selection_state import ActivitySelectionState
+from .analysis_execution_dispatch import (
+    FREQUENT_STARTING_POINTS_MODE,
+    HEATMAP_MODE,
+    dispatch_analysis_request,
+)
 from .analysis_models import RunAnalysisRequest, RunAnalysisResult
-from .analysis_result_builder import build_empty_analysis_result
-
-FREQUENT_STARTING_POINTS_MODE = "Most frequent starting points"
-HEATMAP_MODE = "Heatmap"
 
 
 class AnalysisController:
@@ -38,31 +39,7 @@ class AnalysisController:
         if request is None:
             request = self.build_request(**legacy_kwargs)
 
-        if request.analysis_mode == FREQUENT_STARTING_POINTS_MODE:
-            return _run_frequent_start_points_analysis(request.starts_layer)
-
-        if request.analysis_mode == HEATMAP_MODE:
-            return _run_activity_heatmap_analysis(
-                activities_layer=request.activities_layer,
-                points_layer=request.points_layer,
-            )
-
-        return build_empty_analysis_result()
+        return dispatch_analysis_request(request)
 
     def run_request(self, request: RunAnalysisRequest) -> RunAnalysisResult:
         return self.run(request=request)
-
-
-def _run_frequent_start_points_analysis(starts_layer):
-    from .frequent_start_points_analysis import run_frequent_start_points_analysis
-
-    return run_frequent_start_points_analysis(starts_layer)
-
-
-def _run_activity_heatmap_analysis(activities_layer=None, points_layer=None):
-    from .activity_heatmap_analysis import run_activity_heatmap_analysis
-
-    return run_activity_heatmap_analysis(
-        activities_layer=activities_layer,
-        points_layer=points_layer,
-    )
---
+++ analysis/application/analysis_execution_dispatch.py
@@ -0,0 +1,19 @@
+from .activity_heatmap_analysis import run_activity_heatmap_analysis
+from .analysis_result_builder import build_empty_analysis_result
+from .frequent_start_points_analysis import run_frequent_start_points_analysis
+
+FREQUENT_STARTING_POINTS_MODE = "Most frequent starting points"
+HEATMAP_MODE = "Heatmap"
+
+
+def dispatch_analysis_request(request):
+    if request.analysis_mode == FREQUENT_STARTING_POINTS_MODE:
+        return run_frequent_start_points_analysis(request.starts_layer)
+
+    if request.analysis_mode == HEATMAP_MODE:
+        return run_activity_heatmap_analysis(
+            activities_layer=request.activities_layer,
+            points_layer=request.points_layer,
+        )
+
+    return build_empty_analysis_result()
