0fdf0c3de1822f1a02738fe8c5c5231c1fdba370
Emmanuel Belo <emmanuel.nicolas.belo@gmail.com>
2026-04-25 20:34:06 +0200

    ui: compose placeholder wizard shell

--- ui/dockwidget/__init__.py
+++ ui/dockwidget/__init__.py
@@ -1,6 +1,14 @@
 """Dock widget components for the qfit wizard shell."""
 
 from .stepper_bar import STEPPER_LABELS, STEPPER_STATES, StepperBar
+from .wizard_composition import WizardShellComposition, build_placeholder_wizard_shell
 from .wizard_shell_presenter import WizardShellPresenter
 
-__all__ = ["STEPPER_LABELS", "STEPPER_STATES", "StepperBar", "WizardShellPresenter"]
+__all__ = [
+    "STEPPER_LABELS",
+    "STEPPER_STATES",
+    "StepperBar",
+    "WizardShellComposition",
+    "WizardShellPresenter",
+    "build_placeholder_wizard_shell",
+]
