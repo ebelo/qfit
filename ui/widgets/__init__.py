3a6cd1fd308215ad08b2326a1264bad06e0f786d
ebelo <emmanuel.belo@camptocamp.com>
2026-04-25 13:30:25 +0200

    ui: use native password field helper

--- ui/widgets/__init__.py
+++ ui/widgets/__init__.py
@@ -1,3 +1,13 @@
-from .compat import checked_list_values, make_checkable_list, make_range_slider
+from .compat import (
+    checked_list_values,
+    make_checkable_list,
+    make_password_line_edit,
+    make_range_slider,
+)
 
-__all__ = ["checked_list_values", "make_checkable_list", "make_range_slider"]
+__all__ = [
+    "checked_list_values",
+    "make_checkable_list",
+    "make_password_line_edit",
+    "make_range_slider",
+]
