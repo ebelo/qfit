09ba596ce624b4864a3f114d4471babf01707514
Emmanuel Belo <emmanuel.nicolas.belo@gmail.com>
2026-05-10 06:44:54 +0200

    refactor: add wizard step-page compat module

--- ui/dockwidget/step_page.py
+++ ui/dockwidget/step_page.py
@@ -559,16 +559,10 @@ def _ghost_button_stylesheet() -> str:
 
 __all__ = [
     "DockWorkflowPageSpec",
-    "DockWizardPageSpec",
     "StepPage",
     "WorkflowStepPage",
-    "WizardStepPage",
     "apply_workflow_step_page_statuses",
-    "apply_wizard_step_page_statuses",
     "build_default_workflow_page_specs",
-    "build_default_wizard_page_specs",
     "build_workflow_step_pages",
-    "build_wizard_step_pages",
     "install_workflow_step_pages",
-    "install_wizard_step_pages",
 ]
